9f1cb07cafbd687b0a51f47b76824dda2ff109d0
zhen <khcy5hzx@nottingham.edu.my>
2016-12-06 11:15:44 +0800

    Initial pre-release, to be pulled

---
+++ public/controllers/controller.js
@@ -0,0 +1,49 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+
+
+var refresh = function() {
+  $http.get('/publisher').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+    $scope.contact = "";
+  });
+};
+
+refresh();
+
+$scope.addContact = function() {
+  console.log($scope.contact);
+  $http.post('/publisher', $scope.contact).success(function(response) {
+    console.log(response);
+    refresh();
+  });
+};
+
+$scope.remove = function(name) {
+  console.log(name);
+  $http.delete('/publisher/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/publisher/' + id).success(function(response) {
+    $scope.contact = response;
+  });
+};  
+
+$scope.update = function() {
+  console.log($scope.contact._id);
+  $http.put('/publisher/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh();
+  })
+};
+
+$scope.deselect = function() {
+  $scope.contact = "";
+}
+
+}]);﻿
---
+++ server.js
@@ -0,0 +1,57 @@
+// MEAN Stack RESTful API Tutorial - Contact List App
+
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('publisher', ['publisher']);
+var bodyParser = require('body-parser');
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/publisher', function (req, res) {
+  console.log('I received a GET request');
+
+  db.publisher.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
+});
+
+app.post('/publisher', function (req, res) {
+  console.log(req.body);
+  db.publisher.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.delete('/publisher/:_id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.publisher.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.get('/publisher/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(id);
+  db.publisher.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
+    res.json(doc);
+  });
+});
+
+app.put('/publisher/:id', function (req, res) {
+  var id = req.params.id;
+  console.log(req.body.name);
+  db.publisher.findAndModify({
+    query: {_id: mongojs.ObjectId(id)},
+    update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+    new: true}, function (err, doc) {
+      res.json(doc);
+    }
+  );
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
